b713ed48f701b1b30db8008eb98be11fd20fabd6
Robert Zita <44588710+robertzita@users.noreply.github.com>
2019-01-24 20:27:40 +0100

    dodavanje <td>

--- PDO Završni vježba/novi.php
+++ PDO Završni vježba/novi.php
@@ -1,8 +1,13 @@
 <form action="dodajNovi.php" method="post">
-    <input type="text" name="Naziv" />
+<td>Naziv </td>
+    <input type="text" name="naziv" />
+<td>Adresa </td>
     <input type="text" name="adresa" />
+<td>Poštanski Broj </td>
     <input type="text" name="postanskibroj" />
+<td>Broj telefona </td>    
     <input type="text" name="brojtelefona" />
+<td>Email </td>
     <input type="text" name="email" />
     <input type="submit" value="Dodaj" />
 </form>
