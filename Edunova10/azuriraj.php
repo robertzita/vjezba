15d20752c740522ec0195c2a3ecb37662d2fccf6
Robert Zita <44588710+robertzita@users.noreply.github.com>
2019-01-24 17:33:28 +0100

    ispravak update

--- Edunova10/azuriraj.php
+++ Edunova10/azuriraj.php
@@ -1,15 +1,31 @@
+<?php 
+include_once "PDO.php";
+
+$izraz=$db->prepare("
+
+        select * from smjer where sifra=:sifra ;
+                
+
+        ");
+
+$izraz->execute($_GET);
+$smjer = $izraz->fetch(PDO::FETCH_OBJ);
+
+
+?>
 <form action="azurirajNovi.php" method="post">
  <tr>
         <th>Naziv</th>
-    <input type="text" name="naziv" />
+    <input type="text" name="naziv" value="<?php echo $smjer->naziv ?>" />
         <th>Trajanje</th>
-    <input type="text" name="trajanje" />
+    <input type="text" name="trajanje" value="<?php echo $smjer->trajanje ?>" />
         <th>Cijena</th>
-    <input type="text" name="cijena" />
+    <input type="text" name="cijena" value="<?php echo $smjer->cijena ?>" />
         <th>Upisnina</th>
-    <input type="text" name="upisnina" />
+    <input type="text" name="upisnina" value="<?php echo $smjer->upisnina ?>" />
         <th>Verificiran</th>
-    <input type="text" name="verificiran" />
+    <input type="text" name="verificiran" value="<?php echo $smjer->verificiran ?>" />
+    <input type="hidden" name="sifra" value="<?php echo $smjer->sifra ?>" />
 </tr>
     <input type="submit" value="Ažuriraj" />
 </form>
